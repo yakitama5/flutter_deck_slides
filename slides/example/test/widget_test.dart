123e2d5a75a047032ccd6e091453c5e577041ad9
Yakuran <rittitarou@yahoo.co.jp>
2026-08-23 18:08:27 +0900

    test: cover consecutive storybook page turns

--- slides/example/test/widget_test.dart
+++ slides/example/test/widget_test.dart
@@ -1,9 +1,23 @@
 import 'package:example_slide/main.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('ExampleApp builds without error', (tester) async {
+  testWidgets('ExampleApp moves through every storybook page', (tester) async {
     await tester.pumpWidget(const ExampleApp());
     await tester.pumpAndSettle();
+
+    expect(find.text('FlutterDeck Storybook'), findsOneWidget);
+
+    await tester.sendKeyEvent(LogicalKeyboardKey.arrowRight);
+    await tester.pumpAndSettle();
+
+    expect(find.textContaining('ある日、青い小鳥は'), findsOneWidget);
+
+    await tester.sendKeyEvent(LogicalKeyboardKey.arrowRight);
+    await tester.pumpAndSettle();
+
+    expect(find.text('おしまい'), findsOneWidget);
+    expect(tester.takeException(), isNull);
   });
 }
