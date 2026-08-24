15f4d801de795f225ef13df9541f6b3aad48c7b5
Yakuran <rittitarou@yahoo.co.jp>
2026-08-24 11:57:47 +0900

    test: 継続アニメーションを固定時間で検証する

--- slides/202607_claudenotebooklm/test/widget_test.dart
+++ slides/202607_claudenotebooklm/test/widget_test.dart
@@ -5,7 +5,7 @@ void main() {
   testWidgets('ClaudenotebooklmApp builds without error', (tester) async {
     await tester.pumpWidget(const ClaudenotebooklmApp());
     await tester.pump();
-    await tester.pumpAndSettle();
+    await tester.pump(const Duration(seconds: 2));
 
     expect(tester.takeException(), isNull);
   });
